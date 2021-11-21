0c37c1c53448d72bb7571a830c019f1d36d4d5e3
Katherine Li <catherine.cli37@gmail.com>
2021-11-21 17:23:51 -0600

    setup database connection to MongoDB Atlas

--- server/index.js
+++ server/index.js
@@ -1,4 +1,23 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-import bodyParser from "body-parser";
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+// import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+// app.use('/posts', postRoutes);
+// https://www.mongodb.com/cloud/atlas
+const CONNECTION_URL = 'mongodb+srv://admin:Ab7ezkeTf@cluster0.qn4zx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .catch((error) => console.log(`${error} did not connect`));
+
+// mongoose.set('useFindAndModify', false);
